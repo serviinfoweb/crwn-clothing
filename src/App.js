3b14458b68d5deb16f5a2ee64ef4fe8d77ff6dc4
Serviinfoweb <serviinfoweb@gmail.com>
2021-03-06 00:41:35 +1100

    update our  menu-item to navigate withRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
+import { Switch,Route } from "react-router-dom";
+
+const HatsPage = () =>(
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+);
 
 function App() {
   return (
-    <div > <HomePage/></div>
+    <div > 
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/hats' component={HatsPage} />
+      </Switch>
+    </div>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -2,11 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+//Library to use navigation inside the site 
+import { BrowserRouter } from "react-router-dom";
 
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
