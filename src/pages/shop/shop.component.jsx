9bef17bd96f4d713f6b656df6f289f04e2294286
Serviinfoweb <serviinfoweb@gmail.com>
2021-03-12 12:23:49 +1100

    Adding header component and link to home , shop page and contact

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import SHOP_DATA from './shop.data';
 import CollectionPreview from '../../components/collection-preview/collection-preview.component';
 
+
 class ShopPage extends React.Component{
     constructor(props){
         super();
@@ -11,12 +12,15 @@ class ShopPage extends React.Component{
     }
     render(){
         const  {collection} = this.state;
-        return (<div className='shop-page'>
-            {
-                collection.map(({id,...otherColletionProps})=>(
-                    <CollectionPreview key={id} {...otherColletionProps}/>
-                ))
-            }
+        return (
+            
+            <div className='shop-page'>
+                
+                {
+                    collection.map(({id,...otherColletionProps})=>(
+                        <CollectionPreview key={id} {...otherColletionProps}/>
+                    ))
+                }
             </div>);
         
     }
